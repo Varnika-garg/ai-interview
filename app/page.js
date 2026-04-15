381c92faab3db9c7e62297a1bb310c46c6886949
Varnika Garg <gargvarnikaa7@gmail.com>
2026-04-15 15:12:38 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { Button } from "../components/ui/button"
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-   <div>
-      <h2>Hello</h2>
-      <Button>Subscribe</Button>
-    </div>
-  );
+  redirect("/dashboard");
 }
