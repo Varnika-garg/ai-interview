cf7640d9951401814f201394ae7881492d5ff55e
Varnika Garg <gargvarnikaa7@gmail.com>
2026-04-25 02:23:58 +0530

    fix get-interview api

--- app/api/get-interview/route.js
+++ app/api/get-interview/route.js
@@ -6,34 +6,19 @@ import { NextResponse } from "next/server";
 export const runtime = "nodejs";
 
 export async function GET(req) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const email = searchParams.get("email");
+  const db = getDb();
 
-    console.log("EMAIL:", email);
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
 
-    if (!email) {
-      return NextResponse.json(
-        { error: "Email missing" },
-        { status: 400 }
-      );
-    }
-
-    const db = getDb();
-
-    const result = await db
-      .select()
-      .from(MockInterview)
-      .where(eq(MockInterview.createdBy, email));
-
-    return NextResponse.json(result);
-
-  } catch (error) {
-    console.error("API ERROR:", error);
-
-    return NextResponse.json(
-      { error: error.message },
-      { status: 500 }
-    );
+  if (!email) {
+    return NextResponse.json({ error: "Email missing" }, { status: 400 });
   }
+
+  const result = await db
+    .select()
+    .from(MockInterview)
+    .where(eq(MockInterview.createdBy, email));
+
+  return NextResponse.json(result);
 }
